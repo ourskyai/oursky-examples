e2e03b467ff5be978ae744a2adb35e942b0563ad
Connor Poole <connor.poole@gmail.com>
2024-06-14 12:48:21 -0700

    adding script to get osrs for norad ids (#4)
    * adding script to get osrs for norad ids
    
    * write to folder

--- python-poetry-sda-basic/oursky_sda/get_osrs.py
+++ python-poetry-sda-basic/oursky_sda/get_osrs.py
@@ -4,44 +4,62 @@ import ourskyai_sda_api
 from pprint import pprint
 from dotenv import load_dotenv
 import typing
-from datetime import timedelta
+from typing import List  
+from datetime import timedelta, date
+from json import dumps
 
 load_dotenv()  # take environment variables from .env.
-
 
 # Configure Bearer authorization: BearerToken
 configuration = ourskyai_sda_api.Configuration(
     access_token = os.environ["OURSKY_API_TOKEN"],
 )
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+norad_ids = ["25875"]  # List[str] | A list of NORAD IDs of the satellite targets to fetch.
+write_to_disk = True # bool | If true, the response will be written to a file. (optional) (default to False)
+from_time = datetime.datetime.now(datetime.timezone.utc) - timedelta(days=1)  # datetime | The start time of the observation sequence results to fetch. (optional)
+
+# create osrs directory if it does not exist
+if write_to_disk:
+    if not os.path.exists("osrs"):
+        os.makedirs("osrs")
+
 # Enter a context with an instance of the API client
 with ourskyai_sda_api.ApiClient(configuration) as api_client:
     api_instance = ourskyai_sda_api.DefaultApi(api_client)
 
-    # Get the ISS target
-    targets = api_instance.v1_get_satellite_targets(norad_id="25544").targets
-    pprint(targets)
+    for norad_id in norad_ids:
+        targets = api_instance.v1_get_satellite_targets(norad_id=norad_id).targets
 
-    iss = typing.cast(ourskyai_sda_api.V1SatelliteTarget, targets[0])
+        target = typing.cast(ourskyai_sda_api.V1SatelliteTarget, targets[0])
 
-    # fetch the upcoming potential observation windows for the iss
-    # Add two days to the current timestamp
-    new_timestamp = datetime.datetime.now() + timedelta(days=2)
-    upcoming_passes = api_instance.v1_get_satellite_potentials(satellite_target_id=iss.id, until=new_timestamp)
-    pprint(upcoming_passes)
+        print(f"Fetching OSRs for ({target.norad_id})")
 
-    # add a task request to observe the iss
-    try:
-        request = ourskyai_sda_api.V1CreateOrganizationTargetRequest(
-            satelliteTargetId= targets[0].id,
-        )
-        response = api_instance.v1_create_organization_target(request)
-    except:
-        print("Failed to create organization target")
+        # fetch the OSRs for the organization
+        osrs: List[ourskyai_sda_api.V1ObservationSequenceResult] = []
+        more = True
+        after_time = None
+        while more:
+            batch = typing.cast(typing.List[ourskyai_sda_api.V1ObservationSequenceResult], api_instance.v1_get_observation_sequence_results(target_id=target.id, after=after_time))
+            filtered = [osr for osr in batch if from_time <= osr.created_at]
+            print(f"Fetched {len(filtered)} additional OSRs")
+            osrs.extend(filtered)
+            if len(filtered) != 5:
+                more = False
+            else:
+                after_time = batch[-1].created_at
 
-    # fetch the OSRs for the organization
-    osrs = api_instance.v1_get_observation_sequence_results(target_id=iss.id) 
-    for osr in osrs:
-        print(osr)
-        for image_set in osr.image_sets:
-            api_instance.v1_get_node_properties(image_set.node_id)
+        print(f"Fetched {len(osrs)} total OSRs for ({target.norad_id})")
+        for osr in osrs:
+            if write_to_disk:
+                with open(f"osrs/{norad_id}_{osr.created_at}.json", "w") as f:
+                    f.write(dumps(osr.to_dict(), default=json_serial, indent=4, sort_keys=True))
+            else:
+                print(osr.to_str())
