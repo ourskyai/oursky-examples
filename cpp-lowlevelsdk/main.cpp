a1e2f539c83f30e4bde160dc0d0d70ffdc577d49
Jason Wang <jason.w162@gmail.com>
2025-06-04 18:00:48 -0700

    Update cpp-lowlevelsdk default minimumIntervalMicroseconds to 0

--- cpp-lowlevelsdk/main.cpp
+++ cpp-lowlevelsdk/main.cpp
@@ -7,7 +7,8 @@
 
 int main() {
     OSLowLevelSdkClient client("INSERT_CONTROLLER_IP_HERE:50099");
-    client.StreamObservatoryStatus(1000, 1000, [](oslowlevelsdk::V1ObservatoryStatus status) {
+    // 0 minimumIntervalMicroseconds yields a message as soon as it's available
+    client.StreamObservatoryStatus(0, 1000, [](oslowlevelsdk::V1ObservatoryStatus status) {
         std::cout << "Timestamp: " << status.timestamp().seconds()  << "." << status.timestamp().nanos() << std::endl;
         std::cout << "Primary Mount Encoder: " << status.encoder_mount_primary_radians() << " radians" << std::endl;
         std::cout << "Secondary Mount Encoder: " << status.encoder_mount_secondary_radians() << " radians" << std::endl;
