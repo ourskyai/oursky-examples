10681d2588bbbe9a4ccbf2b3a92848ca025e34cf
Kevin Ivarsen <kivarsen@planewave.com>
2025-06-20 15:35:26 -0700

    * Add command line parameters for specifying certs, controller ID, and other options * Prevent stream from auto-closing after 120 minutes * Attempt to report error message if stream fails to start

--- cpp-lowlevelsdk/main.cpp
+++ cpp-lowlevelsdk/main.cpp
@@ -2,23 +2,69 @@
 #include <iostream>
 #include <cstdio>
 #include <chrono>
+#include <filesystem>
+#include <CLI/CLI.hpp>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: grpc_client [ipaddress:port]" << std::endl;
+    CLI::App app{"gRPC Client for Observatory Status"};
+    app.formatter(std::make_shared<CLI::Formatter>());
+
+    std::string serverAddress;
+    app.add_option("server", serverAddress, "API server address")->required();
+    
+    int port = 50099;
+    app.add_option("--port", port, "API server port number (default: 50099)");
+
+    int minIntervalMicrosec = 0;
+    app.add_option("--min-interval", minIntervalMicrosec, 
+        "Minimum interval between streamed messages, in microseconds\n"
+        "(default: 0 = as fast as possible)");
+    
+    int timeoutMillisec = 10000;
+    app.add_option("--timeout", timeoutMillisec, "Timeout in milliseconds (default: 10000)");
+    
+
+    std::string certRootPath = "./certs";
+    app.add_option("--cert-dir", certRootPath, "Path to the directory containing certificates and key")->default_str("./certs");
+    
+    std::string nodeControllerId;
+    app.add_option("--controller-id", nodeControllerId, 
+        "The UUID of the node controller.\n"
+        "TIP: You can read this from your certificate file as follows:\n"
+        "openssl x509 -in client_cert.pem -noout -text | grep nodeControllerId\n"
+    )->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::string rootCertPath = certRootPath + "/root_cert.pem";
+    std::string clientCertPath = certRootPath + "/client_cert.pem";
+    std::string clientKeyPath = certRootPath + "/client_key.pem";
+
+    if (!std::filesystem::exists(rootCertPath)) {
+        std::cerr << "Root certificate file not found: " << rootCertPath << std::endl;
         return 1;
     }
 
-    std::cerr << "Connecting" << std::endl;
-    OSLowLevelSdkClient client(argv[1]);
-    std::cerr << "Connected" << std::endl;
+    if (!std::filesystem::exists(clientCertPath)) {
+        std::cerr << "Client certificate file not found: " << clientCertPath << std::endl;
+        return 1;
+    }
+
+    if (!std::filesystem::exists(clientKeyPath)) {
+        std::cerr << "Client key file not found: " << clientKeyPath << std::endl;
+        return 1;
+    }
+
+    std::string serverAddressWithPort = serverAddress + ":" + std::to_string(port);
+
+    std::cerr << "Connecting to " << serverAddressWithPort << "..." << std::endl;
+    OSLowLevelSdkClient client(serverAddressWithPort, rootCertPath, clientCertPath, clientKeyPath, nodeControllerId);
 
     std::cout << "# NumReceivedMessages, DC Time (usec since 1970), Received time (usec since 1970), Timestamp Age (usec), Timestamp Interval (msec)\n";
+
     double lastMessageTimeSeconds = 0;
-    int minInterval = 0;
-    int timeout = 10000;
     int messageCount = 0;
-    client.StreamObservatoryStatus(minInterval, timeout, [&lastMessageTimeSeconds, &messageCount](oslowlevelsdk::V1ObservatoryStatus status) {
+    client.StreamObservatoryStatus(minIntervalMicrosec, timeoutMillisec, [&lastMessageTimeSeconds, &messageCount](oslowlevelsdk::V1ObservatoryStatus status) {
         auto receivedSystemTimeNow = std::chrono::system_clock::now();
         auto receivedSystemTimeUsec = std::chrono::duration_cast<std::chrono::microseconds>(receivedSystemTimeNow.time_since_epoch()).count();
 
